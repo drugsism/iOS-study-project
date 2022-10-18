d89d98d648ccb34b092ce61e6cc38ffdc210c660
drugsism <drugsism@gmail.com>
2022-10-18 17:59:07 +0900

    Configure UI in Diary APP

---
+++ Diary/Diary/DiaryDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DiaryDetailViewController.swift
+//  Diary
+//
+//  Created by Breeze on 2022/10/18.
+//
+
+import UIKit
+
+class DiaryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+		
+		
+    }
+
+}
